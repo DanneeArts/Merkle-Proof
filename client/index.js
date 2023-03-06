6c725ef87a2a7653e5acfd5d22a38e2d99a9e957
DanneeArts <sagaciousdaniel@gmail.com>
2023-03-06 18:36:59 +0100

    initial commit

--- client/index.js
+++ client/index.js
@@ -1,14 +1,28 @@
 const axios = require('axios');
 const niceList = require('../utils/niceList.json');
 const MerkleTree = require('../utils/MerkleTree');
+const prompt = require('prompt-sync')();
+
 
 const serverUrl = 'http://localhost:1225';
 
 async function main() {
   // TODO: how do we prove to the server we're on the nice list? 
+  const merkleTree = new MerkleTree(niceList);
+  const root = merkleTree.getRoot();
+
+  const name = prompt("Input your name to check if its on the list: - ");
+  if(name == null){
+    alert("You have to type a name");
+  }
+  // Find the index of the name and get proof from merkle tree.
+  const index = niceList.findIndex(n => n === name);
+  const proof = merkleTree.getProof(index);
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
     // TODO: add request body parameters here!
+    name,
+    proof
   });
 
   console.log({ gift });
